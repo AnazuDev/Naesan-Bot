5b3bcddf81c8b31ccb43f4ff28e8e1b8486133e0
AnazuDev <AnazuDev>
2023-04-23 13:35:43 +0700

    Webhook

--- public/hook.php
+++ public/hook.php
@@ -3,7 +3,7 @@
 define('BOT_TOKEN', '6093828826:AAGzDvezH2YmdvfRPLuAfcU9LiBzA5WC2iw');
 
 $bot_url = 'https://api.telegram.org/bot' . BOT_TOKEN . '/';
-$webhook_url = 'https://naesan-bot.vercel.app/public/hook.php';
+$webhook_url = 'https://naesan-bot.vercel.app/hook.php';
 
 $url = $bot_url . 'setWebhook?url=' . $webhook_url;
 
